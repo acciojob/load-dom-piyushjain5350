b26a244907231e10d12abbb64f49ab9841066a78
Piyush Jain <126599802+piyushjain5350@users.noreply.github.com>
2023-12-27 22:22:46 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,6 @@
 //your JS code here. If required.
-document.querySelector("#load").innerHTML="DOM load success";
+// document.querySelector("#load").innerHTML="DOM load success";
+function onDOMLoad() {
+        document.body.textContent = "DOM load success";
+      }
+      document.addEventListener("DOMContentLoaded", onDOMLoad);
